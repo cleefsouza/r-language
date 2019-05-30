b084f59b02bb6e7d5c59c0e3559eee1d6c0b3b89
ARYOSVALLDO CLEEF de SOUZA <x10487855485@tcu.gov.br>
2019-05-30 16:52:24 -0300

    instalando packages

---
+++ instalando_packages.r
@@ -0,0 +1,10 @@
+# como importar arquivos
+
+# via CRAN
+install.packages("nome_package")
+
+# via Github
+devtools::install_github("nome_repo/nome_package")
+
+# via zip file
+install.packages("caminho_zip", repos = NULL)
